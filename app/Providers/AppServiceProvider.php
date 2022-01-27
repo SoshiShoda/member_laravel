42ba85873934d987a7e94ac9122cf3c839250798
SoshiShoda <soshi731@gmail.com>
2022-01-27 14:40:36 +0900

    heroku対応

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     {
         \Illuminate\Support\Facades\Schema::defaultStringLength(191);
 
-        if (\App::environment(['production']) || \App::environment(['develop'])) {
+        if (\App::environment(['production']) {
             \URL::forceScheme('https');
     }
 }
